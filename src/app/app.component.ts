6fcc08fbb1d17bc8315ea8005a8349b71948efae
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-01 14:20:45 -0400

    When the user clicks a hero, the style of the li changes to be selected.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,19 +22,22 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
       <h1>{{title}}</h1>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
       <h2>My Heroes</h2>
-      <ul class="heroes">
-          <li *ngFor="let hero of heroes">
-              <span class="badge">{{hero.id}}</span> {{hero.name}}
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes"
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
           </li>
-      </ul>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div><label>name: </label>{{hero.name}}</div>
+        </ul>
+        <div *ngIf="selectedHero">
+          <h2>{{selectedHero.name}} details!</h2>
+          <div><label>id: </label>{{selectedHero.id}}</div>
+          <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+          </div>
+        </div>
       `,
       styles: [`
       .selected {
@@ -89,9 +92,10 @@ const HEROES: Hero[] = [
 
 export class AppComponent  { 
     title: 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
     heroes = HEROES;
+    
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;    
+    }
 }
