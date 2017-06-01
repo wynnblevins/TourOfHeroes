34648514b8a88f7cb4e2d90e57869ba601f2beb1
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-01 10:56:09 -0400

    Initial commit of Angular 2 tour of heroes application.

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+    id: number;
+    name: string;    
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+      <h1>{{title}}</h1>
+      <div>
+          <label>name: </label>
+          <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div><label>name: </label>{{hero.name}}</div>
+      `
+})
+
+export class AppComponent  { 
+    title: 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
