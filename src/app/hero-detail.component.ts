cc1b4d32d3c77e8a7e6df72a114cc37f15bc6f19
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-02 14:39:00 -0400

    Top heroes are now displayed on the dashboard.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,34 +1,31 @@
-// Keep the Input import for now, you'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
-import { HeroService } from './hero.service';
-
-import { Hero } from './hero';
 import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
+import { Hero }         from './hero';
+import { HeroService }  from './hero.service';
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
+  styleUrls: [ './hero-detail.component.css' ]
 })
-
 export class HeroDetailComponent implements OnInit {
-    @Input() hero: Hero
-    
-    constructor(
-        private heroService: HeroService,
-        private route: ActivatedRoute,
-        private location: Location
-    ) {}
-    
-    ngOnInit(): void {
-        this.route.params
-            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
-            .subscribe(hero => this.hero = hero);
-    }
-    
-    goBack(): void {
-        this.location.back();
-    }
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
