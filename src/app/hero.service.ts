8dff3c645b7b80255024601a284d06a1ef41087f
Wynn Blevins <wblevins@ibcdbs.com>
2017-06-05 08:54:44 -0400

    changed getHero method to only return one hero rather than getting all heroes and filtering by id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,13 +16,17 @@ export class HeroService {
                .catch(this.handleError);
   }
   
+  getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+      
+    return this.http.get(url)
+        .toPromise()
+        .then(response => response.json().data as Hero)
+        .catch(this.handleError);
+  }
+      
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
-  }
-
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
